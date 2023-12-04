9ec459cd8966552566922dcd13902040e8eb8374
Samuel Gonzalez <sadagonzagi@gmail.com>
2023-12-04 19:10:45 -0400

    push

---
+++ ok/js/test_1.js
@@ -0,0 +1,13 @@
+// let num = 0
+// let num2 = 5
+
+// console.log(num + num2)
+// console.log(num * num2)
+// console.log(num - num2)
+// console.log(num / num2)
+
+let boton = document.getElementById("btn-r")
+
+boton.addEventListener("click", () => {
+    window.alert('pelon')
+})
